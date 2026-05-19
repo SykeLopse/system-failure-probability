e23f3b3f349af9e7f04440188cfb4311361269af
SykeLopse <iamshoaibmir@gmail.com>
2026-05-19 19:00:00 +0000

    Implemented failure calculation processing functions

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 import math
 import matplotlib.pyplot as plt
+
+def calculate_failure_probability(failure_rate, time_hours):
+    reliability = math.exp(-failure_rate * time_hours)
+    return 1 - reliability
