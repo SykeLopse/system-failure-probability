273a57d6485fa832314defa02a7d95df3ab3898d
SykeLopse <iamshoaibmir@gmail.com>
2026-05-20 19:15:00 +0000

    Applied list comprehension sequences for rapid indexing

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ import matplotlib.pyplot as plt
 def calculate_failure_probability(failure_rate, time_hours):
     reliability = math.exp(-failure_rate * time_hours)
     return 1 - reliability
+
+def generate_probability_data(failure_rate, max_hours, step):
+    times = list(range(0, max_hours + step, step))
+    probabilities = [calculate_failure_probability(failure_rate, t) for t in times]
+    return times, probabilities
