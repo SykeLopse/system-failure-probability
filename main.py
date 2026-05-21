cda36b048f3d13c83cc65e4e7b77977530e27993
SykeLopse <iamshoaibmir@gmail.com>
2026-05-21 19:15:00 +0000

    Constructed command-line logging interface parameters

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ def generate_probability_data(failure_rate, max_hours, step):
     times = list(range(0, max_hours + step, step))
     probabilities = [calculate_failure_probability(failure_rate, t) for t in times]
     return times, probabilities
+
+if __name__ == '__main__':
+    SYSTEM_FAILURE_RATE = 0.015
+    MAX_HOURS = 200
+    STEP = 10
+    t_data, p_data = generate_probability_data(SYSTEM_FAILURE_RATE, MAX_HOURS, STEP)
+    print('Time (Hrs) | Failure Prob')
+    for t, p in zip(t_data, p_data):
+        print(f'{t:<11} | {p:.4f}')
